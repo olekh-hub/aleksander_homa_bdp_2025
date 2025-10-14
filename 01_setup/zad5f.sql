647d51fd4f8d3f58c171b4c7d9049471e7d65fc4
aleksanderhoma <118051412+olekh-hub@users.noreply.github.com>
2025-10-14 22:09:23 +0200

    Update zad5f.sql

--- 01_setup/zad5f.sql
+++ 01_setup/zad5f.sql
@@ -1,1 +1,5 @@
+select prac.imie, prac.nazwisko, (godz.liczba_godzin - 160) as liczba_nadgodzin from ksiegowosc.wynagrodzenie w
+join ksiegowosc.pracownicy prac on prac.id_pracownika = w.id_pracownika
+join ksiegowosc.godziny godz on godz.id_godziny = w.id_godziny
+where godz.liczba_godzin > 160
 
