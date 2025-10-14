ab974ef24d31f1d5ca1ba795bf672f34ee1fa214
aleksanderhoma <118051412+olekh-hub@users.noreply.github.com>
2025-10-14 22:04:27 +0200

    Update zad5g.sql

--- 01_setup/zad5g.sql
+++ 01_setup/zad5g.sql
@@ -1,5 +1,1 @@
--- 5g) tu też zmieniłem przedział, żeby były jakieś rekordy
-select imie, nazwisko from ksiegowosc.pracownicy prac 
-join ksiegowosc.wynagrodzenie w on w.id_pracownika = prac.id_pracownika
-join ksiegowosc.pensja pe on w.id_pensji = pe.id_pensji
-where pe.kwota::numeric between 5000 and 6000
+
