64cde93a1d140b90bb6e966d81b755e5fecbc7f4
aleksanderhoma <118051412+olekh-hub@users.noreply.github.com>
2025-10-14 22:03:37 +0200

    Update zad5b.sql

--- 01_setup/zad5b.sql
+++ 01_setup/zad5b.sql
@@ -1,5 +1,1 @@
--- 5b) zmienilem na 5500 zeby pasowalo 
-select id_pracownika from ksiegowosc.wynagrodzenie w
-join ksiegowosc.pensja pe on pe.id_pensji = w.id_pensji 	
-join ksiegowosc.premia pr on pr.id_premii = w.id_premii
-where (pe.kwota + pr.kwota)::numeric > 5500
+
