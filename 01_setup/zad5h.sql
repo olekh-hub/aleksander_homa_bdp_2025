0e5e1ec7d0aa205cd0ddd7192b586f5d23f6357e
aleksanderhoma <118051412+olekh-hub@users.noreply.github.com>
2025-10-14 22:04:38 +0200

    Remove SQL query from zad5h.sql

--- 01_setup/zad5h.sql
+++ 01_setup/zad5h.sql
@@ -1,5 +1,1 @@
-select prac.imie, prac.nazwisko from ksiegowosc.wynagrodzenie w
-join ksiegowosc.pracownicy prac on prac.id_pracownika = w.id_pracownika
-join ksiegowosc.premia pr on pr.id_premii = w.id_premii
-join ksiegowosc.godziny godz on godz.id_godziny = w.id_godziny
-where pr.kwota::numeric = 0 and godz.liczba_godzin > 160
+
