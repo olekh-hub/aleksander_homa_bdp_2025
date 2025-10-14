194d5423e5fd2c66c45f84cb7c94e09f259e9d5c
aleksanderhoma <118051412+olekh-hub@users.noreply.github.com>
2025-10-14 22:10:02 +0200

    Update zad5i.sql

--- 01_setup/zad5i.sql
+++ 01_setup/zad5i.sql
@@ -1,1 +1,4 @@
-
+select prac.imie, prac.nazwisko, pe.kwota from ksiegowosc.pracownicy prac
+join ksiegowosc.wynagrodzenie w on w.id_pracownika = prac.id_pracownika
+join ksiegowosc.pensja pe on w.id_pensji = pe.id_pensji
+order by pe.kwota desc
