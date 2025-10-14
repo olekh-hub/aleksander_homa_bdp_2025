5942390cb006917fa6e88249d821e3a6607b6886
aleksanderhoma <118051412+olekh-hub@users.noreply.github.com>
2025-10-14 22:08:46 +0200

    Update zad5c.sql

--- 01_setup/zad5c.sql
+++ 01_setup/zad5c.sql
@@ -1,1 +1,5 @@
+select id_pracownika from ksiegowosc.wynagrodzenie w
+join ksiegowosc.pensja pe on pe.id_pensji = w.id_pensji 	
+join ksiegowosc.premia pr on pr.id_premii = w.id_premii
+where pr.kwota::numeric = 0 and pe.kwota::numeric > 2000
 
